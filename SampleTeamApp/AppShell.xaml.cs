7e7aed919cff8cd5f3f4e099c6af8c68ebe42c63
Lakshitha Attanayaka <lakshitha.attanayaka@gmail.com>
2022-07-17 16:14:57 +0530

    Add project files.

---
+++ SampleTeamApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleTeamApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SampleTeamApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SampleTeamApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
